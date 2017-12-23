91d380d391034c228e3038e690d0567218008a26
Alex Bowen <ajbowen249@gmail.com>
2017-12-23 13:30:04 -0500

    update node server and defaul to port 8081

--- standalone.js
+++ standalone.js
@@ -2,5 +2,5 @@ const coreServer = require('bplc-node-server');
 const FakeCore = require('./fakeCore').FakeCore;
 
 const fakeCore = new FakeCore();
-const server = new coreServer.server(fakeCore);
+const server = new coreServer.server(fakeCore, 8081);
 server.init();
